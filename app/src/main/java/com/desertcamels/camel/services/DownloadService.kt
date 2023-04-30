5bad84dbaf084ab3fa8eeb966c0492ef8fd19af3
Abbas Abdullah <abbasaleid@gmail.com>
2023-04-30 20:09:44 +0300

    notification content intent

--- app/src/main/java/com/desertcamels/camel/services/DownloadService.kt
+++ app/src/main/java/com/desertcamels/camel/services/DownloadService.kt
@@ -1,15 +1,14 @@
 package com.desertcamels.camel.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
+import android.app.PendingIntent.FLAG_IMMUTABLE
 import android.content.Intent
 import android.os.Environment
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.desertcamels.camel.MainActivity
 import com.desertcamels.camel.MainActivityState
 import com.desertcamels.camel.R
 import com.desertcamels.camel.models.MainViewModel
@@ -28,11 +27,15 @@ class DownloadService : Service() {
     private lateinit var notificationManager: NotificationManagerCompat
     private lateinit var mainViewModel: MainViewModel
     private lateinit var scope: CoroutineScope
+    private lateinit var mainIntent: Intent
+    private lateinit var mainPendingIntent: PendingIntent
     override fun onCreate() {
         super.onCreate()
         mainViewModel = MainViewModel()
         notificationManager = NotificationManagerCompat.from(this)
         scope = CoroutineScope(Dispatchers.IO)
+        mainIntent = Intent(this, MainActivity::class.java)
+        mainPendingIntent = PendingIntent.getActivity(this, 1, mainIntent, FLAG_IMMUTABLE)
         try {
             YoutubeDL.getInstance().init(this)
         } catch (e: YoutubeDLException) {
@@ -84,7 +87,7 @@ class DownloadService : Service() {
             .setContentText(status)
             .setSmallIcon(R.drawable.baseline_download_24)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setAutoCancel(true)
+            .setContentIntent(mainPendingIntent)
 
         val channel = NotificationChannel(
             "Download Channel",
